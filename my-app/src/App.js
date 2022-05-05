5446e674d1a48f41b4f007c8b9dda4a1ae961cf6
MattiBSH <cph-mh829@cphbusiness.dk>
2022-05-05 11:31:54 +0200

    react

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState,useEffect, } from 'react';
+function App() {
+  function retrieveEmp() {
+    
+  }
+  function retrieveDrilldowns() {
+    
+  }
+  return (
+    <div className="App">
+     <h1>Redis Frontend</h1>
+    
+    </div>
+  );
+}
+
+export default App;
